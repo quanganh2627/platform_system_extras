be936d17d26389736d9fa6572448098eb2c95615
Andrew Boie <andrew.p.boie@intel.com>
2012-07-11 14:10:35 -0700

    mkuserimg.sh: Create lost+found directories
    EXT image that lack lost+found won't pass fsck; create this
    before calling make_ext4fs.
    
    Change-Id: Ib5da2105296bb514107e83c8ac1619f3f29061ba
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- ext4_utils/mkuserimg.sh
+++ ext4_utils/mkuserimg.sh
@@ -52,6 +52,9 @@ if [ -n "$FC" ]; then
     FCOPT="-S $FC"
 fi
 
+mkdir -p $SRC_DIR/lost+found
+chmod 700 $SRC_DIR/lost+found
+
 MAKE_EXT4FS_CMD="make_ext4fs $ENABLE_SPARSE_IMAGE $FCOPT -l $SIZE -a $MOUNT_POINT $OUTPUT_FILE $SRC_DIR"
 echo $MAKE_EXT4FS_CMD
 $MAKE_EXT4FS_CMD
